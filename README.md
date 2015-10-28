f2bc7d668478cab44e393d1e899578ce16a86ee1
phi <phi1618jp@gmail.com>
2015-10-29 06:58:55 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # express-twitter-oauth-app
 express-twitter-oauth-app
+
+
+## Access
+
+<http://127.0.0.1:3000/>
---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+ *
+ */
+
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
+var path = require('path');
+var app = express();
+
+// passport
+passport.use(new Strategy({
+  consumerKey: 'aaaaaaaaaaaaaaaa',
+  consumerSecret: 'bbbbbbbbbbbbbbbbbb',
+  callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
+}, function(token, tokenSecret, profile, done) {
+  return done(null, profile);
+}));
+passport.serializeUser(function(user, done) {
+  console.log(user);
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Config
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'hogefoobar', resave: true, saveUninitialized: true }));
+
+// Init Passport and Session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/', function(req, res) {
+  res.render('home', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+app.get('/login/twitter/return',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/profile', function(req, res){
+  res.render('profile', { user: req.user });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
